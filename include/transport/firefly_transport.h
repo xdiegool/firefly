58e7cf09b91bcb1af4a346557e44d10beed962b4
= <gnidde gnidde@cs.lth.se>
2012-07-10 21:13:08 +0200

    Added some docs

--- include/transport/firefly_transport.h
+++ include/transport/firefly_transport.h
@@ -6,6 +6,16 @@
 #ifndef FIREFLY_TRANSPORT_H
 #define FIREFLY_TRANSPORT_H
 
+/**
+ * @brief This callback which will be called when a new connection is received.
+ *
+ * This function is implemented by the application layer. It will be called
+ * when the transport layer receives a new connection. The return value of this
+ * function specifies whether to accept the connection or refuse it, true will
+ * accept it and false will refuse it.
+ * @param conn The received connection
+ * @return Return true to accept the connection and false to refuse it
+ */
 typedef bool (*application_on_conn_recv_cb)(struct connection *conn);
 
 struct llp_connection_list_node {
--- src/transport/firefly_transport_udp_posix_private.h
+++ src/transport/firefly_transport_udp_posix_private.h
@@ -1,12 +1,19 @@
+/**
+ * @file
+ * @brief UDP specific and private transport structures and functions.
+ */
+
 #ifndef FIREFLY_TRANSPORT_UDP_POSIX_PRIVATE_H
 #define FIREFLY_TRANSPORT_UDP_POSIX_PRIVATE_H
 
 /**
  * @brief Find the connection with the specified address.
+ *
  * Find the connection with the specified address ascosiated with
- * the supplied llp. The connection with a matching address is returned,
- * if none is found NULL is returned.
+ * the supplied llp. The connection with a matching address
+ * is returned, if none is found NULL is returned.
  * @param addr The address of the connection to find.
+ * @param llp The LLP ascosiated with the connection.
  * @return The connection with a matching address.
  * @retval NULL Is returned if no connection with a matching address was found.
  */
