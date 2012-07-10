85ec801aa1db8e25e656e43b12bd57fdbf181a89
= <gnidde gnidde@cs.lth.se>
2012-07-10 17:34:19 +0200

    Added a comment

--- src/transport/firefly_transport_udp_posix_private.h
+++ src/transport/firefly_transport_udp_posix_private.h
@@ -2,10 +2,13 @@
 #define FIREFLY_TRANSPORT_UDP_POSIX_PRIVATE_H
 
 /**
- * find_connection_by_addr
- * @param	addr
- * @param	llp
- * @return	struct connection *
+ * @brief Find the connection with the specified address.
+ * Find the connection with the specified address ascosiated with
+ * the supplied llp. The connection with a matching address is returned,
+ * if none is found NULL is returned.
+ * @param addr The address of the connection to find.
+ * @return The connection with a matching address.
+ * @retval NULL Is returned if no connection with a matching address was found.
  */
 struct connection *find_connection_by_addr(struct sockaddr_in *addr,
 		struct transport_llp *llp);
