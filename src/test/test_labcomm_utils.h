ecf3845780b6d15c3ba2ed721415e88a1cdf5789
Oscar Olsson <osse.olsson@gmail.com>
2012-08-06 15:21:26 +0200

    Creating a file for each labcomm packet

--- src/test/test_labcomm_utils.h
+++ src/test/test_labcomm_utils.h
@@ -11,3 +11,6 @@ void handle_labcomm_error(enum labcomm_error error_id, size_t nbr_va_args, ...);
 typedef void (* lc_register_f)(struct labcomm_encoder *enc);
 typedef void (* lc_encode_f)(struct labcomm_encoder *enc, void *data);
 void create_labcomm_files_general(lc_register_f reg, lc_encode_f enc, void *data);
+
+void create_lc_files_name(lc_register_f reg, lc_encode_f enc, void *data,
+		char *name);
