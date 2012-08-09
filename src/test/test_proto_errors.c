528a2f6c18c86482288cee5d3e7cedf138907db8
Erik Westrup <erik.westrup@gmail.com>
2012-08-09 12:26:10 +0200

    Begin test errors in protocol.

---
+++ src/test/test_proto_errors.c
@@ -0,0 +1,29 @@
+#include "test/test_proto_errors.h"
+
+#include "CUnit/Basic.h"
+
+#include <labcomm.h>
+
+#include <stdbool.h>
+
+#include <gen/firefly_protocol.h>
+#include <gen/test.h>
+#include <firefly_errors.h>
+#include "protocol/firefly_protocol_private.h"
+#include <protocol/firefly_protocol.h>
+#include "eventqueue/event_queue.h"
+
+int init_suit_proto_errors()
+{
+
+}
+
+int clean_suit_proto_errors()
+{
+
+}
+
+void test_unexpected_ack()
+{
+
+}
---
+++ src/test/test_proto_errors.h
@@ -0,0 +1,10 @@
+#ifndef TEST_PROTO_ERRORS_H
+#define TEST_PROTO_ERRORS_H
+
+int init_suit_proto_errors();
+
+int clean_suit_proto_errors();
+
+void test_unexpected_ack();
+
+#endif
