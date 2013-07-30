976c5328603a8aac554a60364c639bc005216883
Oscar Olsson <osse.olsson@gmail.com>
2013-07-30 15:38:22 +0200

    Completed doc for common transport

--- include/transport/firefly_transport.h
+++ include/transport/firefly_transport.h
@@ -9,16 +9,9 @@
 #include <protocol/firefly_protocol.h>
 
 /**
- * @brief A opaque general data structure representing a \c link \c layer \c port on the
- * transport layer.
+ * @brief A opaque general data structure representing the common part of a
+ * \c link \c layer \c port on the transport layer.
  */
 struct firefly_transport_llp;
-
-/**
- * @brief A structure used to specify callbacks from the transport layer.
- */
-struct firefly_transport_llp_actions {
-
-};
 
 #endif
--- src/transport/firefly_transport_private.h
+++ src/transport/firefly_transport_private.h
@@ -10,26 +10,50 @@
 #include <stdbool.h>
 #include <protocol/firefly_protocol_private.h>
 
+/**
+ * @defgroup conn_state Connection State Values
+ * @brief The different values the state of a connection may have.
+ */
+/**
+ * @brief Defines the opened state of a connection.
+ * @ingroup conn_state
+ */
 #define FIREFLY_CONNECTION_OPEN (1)
+/**
+ * @brief Defines the closed state of a connection.
+ * @ingroup conn_state
+ */
 #define FIREFLY_CONNECTION_CLOSED (0)
 
-enum firefly_llp_state {FIREFLY_LLP_OPEN, FIREFLY_LLP_CLOSING};
+/**
+ * @brief The different states a llp may have. Used to enable multiple
+ * asynchronous actions which depends on each other.
+ */
+enum firefly_llp_state {
+	FIREFLY_LLP_OPEN, /**< Defines the open state of a \a llp. */
+	FIREFLY_LLP_CLOSING /**< Defines the closed state of a \a llp. */
+};
 
 /**
  * @brief A general data structure representing a link layer port on the
  * transport layer.
  */
 struct firefly_transport_llp {
-	enum firefly_llp_state state;
-	struct llp_connection_list_node *conn_list; /**< A linked list of
-							connections. */
-	void *llp_platspec; /**< Platform, and transport method, specific data.
-				*/
-	protocol_data_received_f protocol_data_received_cb; /** The function which passes data received by the transport layer. Replacable for testability. */
+	enum firefly_llp_state state; /**< The state of the llp. Subject to
+									interpretation of each specific transport
+									layer. */
+	struct llp_connection_list_node *conn_list; /**< A linked list of all
+												  connections. */
+	void *llp_platspec; /**< Platform, and transport method, specific data. */
+	protocol_data_received_f protocol_data_received_cb; /** The function which
+														  passes data received
+														  by the transport
+														  layer. Replacable for
+														  testability. */
 };
 
 /**
- * @brief A data structure representing a linked list of connections.
+ * @brief A data structure representing a simple linked list of connections.
  */
 struct llp_connection_list_node {
 	struct llp_connection_list_node *next;
@@ -37,8 +61,8 @@ struct llp_connection_list_node {
 };
 
 /**
- * @brief Compare a connection to some value. This funktion is used by
- * find_connection to find a connection.
+ * @brief Compares a connection to some value. This funktion is used by
+ * find_connection to test if a connection is the one searched for.
  *
  * @param conn The connection to compare.
  * @param context The data to compare the connection to.
@@ -56,31 +80,48 @@ typedef bool (*conn_eq_f)(struct firefly_connection *conn, void *context);
 void add_connection_to_llp(struct firefly_connection *conn,
 		struct firefly_transport_llp *llp);
 
-struct firefly_connection *remove_connection_from_llp(struct firefly_transport_llp *llp,
+/**
+ * @brief Removes the first \c struct #firefly_connection for which the equals
+ * function returns true.
+ *
+ * @param llp The llp the remove a connection from.
+ * @param conn_eq The function used to determine if any connection is the
+ * connection to remove.
+ * @return The removed connection.
+ * @retval NULL If the connection was not found.
+ * @see find_connection()
+ */
+struct firefly_connection *remove_connection_from_llp(
+		struct firefly_transport_llp *llp,
 		void *context, conn_eq_f conn_eq);
 
 /**
- * @brief Finds the first \c struct #firefly_connection for which the equals
- * funktion returns true.
- *
- * Find the first connection for which the equals funktion returns true
- * associated with the supplied \a llp. The connection which is found is
- * returned, if none is found \c NULL is returned.
+ * @brief Finds the first \c struct #firefly_connection, associated with the
+ * given \a llp, for which the equals function returns true.
  *
  * @param llp The link layer port to search for the #firefly_connection in.
- * @param context The context passed to the equals funktion.
- * @param conn_eq The equals funktion used to compare connections.
+ * @param context The context passed to the equals function.
+ * @param conn_eq The function used to determine if any connection is the
+ * connection searched for.
  * @return The #firefly_connection which is found.
  * @retval NULL is returned if no connection was found.
- * @retval struct #firefly_connection* is if it was found.
  */
 struct firefly_connection *find_connection(struct firefly_transport_llp *llp,
 		void *context, conn_eq_f conn_eq);
 
+/**
+ * @brief An implementation of #conn_eq_f which compares the pointer value.
+ *
+ * @param conn The #firefly_connection to compare to.
+ * @param context A pointer to a #firefly_connection.
+ * @return Where conn and context is equal, pointer comparison.
+ */
 bool firefly_connection_eq_ptr(struct firefly_connection *conn, void *context);
 
 /**
- * @breif Should normally not be used except when testing *only* transport layers.
+ * @brief Replaces the callback called when data is received.
+ *
+ * Should normally not be used except when testing *only* transport layers.
  */
 void replace_protocol_data_received_cb(struct firefly_transport_llp *llp,
 		protocol_data_received_f protocol_data_received_cb);
