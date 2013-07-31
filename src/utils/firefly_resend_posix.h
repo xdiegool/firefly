121ad79147b9d46b55f5adc1d7716373e32da918
Oscar Olsson <osse.olsson@gmail.com>
2013-07-31 12:01:00 +0200

    Added docs to resend queue posix.

--- src/utils/firefly_resend_posix.h
+++ src/utils/firefly_resend_posix.h
@@ -4,56 +4,61 @@
 #include <protocol/firefly_protocol.h>
 
 /**
- * Represents an element in the resend queue.
+ * @brief Represents a packet in the resend queue.
  */
 struct resend_elem {
-	unsigned char *data;
-	size_t size;
-	unsigned char id;
-	struct timespec resend_at;
-	unsigned char num_retries;
-	struct firefly_connection *conn;
-	struct resend_elem *prev;
+	unsigned char *data; /**< The data of the packet. */
+	size_t size; /**< The size of the data. */
+	unsigned char id; /**< The unique identifier of this packet in its queue. */
+	struct timespec resend_at; /**< The absolute time when this packet must be
+								 sent again. */
+	unsigned char num_retries; /**< Number of times left this packet will be
+								 sent until it is removed. */
+	struct firefly_connection *conn; /**< The connection this packet comes from. */
+	struct resend_elem *prev; /**< The next packet in the queue. */
 };
 
 /**
- * The resend queue itself (as a single linked list).
+ * @brief The resend queue itself (as a single linked list).
  */
 struct resend_queue {
-	struct resend_elem *first;
-	struct resend_elem *last;
-	pthread_mutex_t lock;
-	pthread_cond_t sig;
-	unsigned char next_id;
+	struct resend_elem *first; /**< The first element in the queue. */
+	struct resend_elem *last; /**< The last element in the queue. */
+	pthread_mutex_t lock; /**< The lock ensuring mutual exclusion when using the
+							queue. */
+	pthread_cond_t sig; /**< Signal used to signal when new packet is added. */
+	unsigned char next_id; /**< Counter keeping track of IDs. */
 };
 
 /**
- * Allocates memory and initializes the elements in the new resend queue struct.
+ * @brief Allocates memory and initializes the elements in the new resend queue
+ * struct.
  *
  * @warning The memory allocated by this function must be freed by calling
  * #firefly_resend_queue_free, freeing in any other way is undefined behaviour
  * and should be avoided unless you know exactly what you do!
  *
  * @return A pointer to the newly allocated resend queue or NULL on failure.
+ * @retval NULL on failure.
  */
 struct resend_queue *firefly_resend_queue_new();
 
 /**
- * Safely frees the memory held by the provided resend queue and all of its
- * elements.  This function MUST be called in order to avoid memory leaks!
+ * @brief Safely frees the memory held by the provided resend queue and all of
+ * its elements. This function MUST be called in order to avoid memory leaks!
  *
  * @param rq The resend queue to free.
  */
 void firefly_resend_queue_free(struct resend_queue *rq);
 
 /**
- * Adds a new element to the back of the provided resend queue with the
+ * @brief Adds a new element to the back of the provided resend queue with the
  * specified parameters.
  *
  * @param rq      The queue to add the element to.
  * @param data    The data to resend.
  * @param size    The size of the data to resend.
- * @param at      The time to resend at.
+ * @param timeout_ms      The time to wait before resending this packet.
  * @param retries The number of retries before giving up.
  * @param conn    The connection to resend on.
  *
@@ -64,36 +69,67 @@ unsigned char firefly_resend_add(struct resend_queue *rq,
 		unsigned char retries, struct firefly_connection *conn);
 
 /**
- * Removes the element from the queue with the provided ID.
+ * @brief Removes the element from the queue with the provided ID.
+ *
+ * @param rq The resend queue to remove from.
+ * @param id The id of the packet to remove as returned by firefly_resend_add()
+ * @see firefly_resend_add()
  */
 void firefly_resend_remove(struct resend_queue *rq, unsigned char id);
 
 /**
- * @brief TODO
+ * @brief Free's a packet including its data.
+ *
+ * @param re The packet to free.
  */
 void firefly_resend_elem_free(struct resend_elem *re);
 
 /**
- * Returns, but does not remove, the first element from the queue.
+ * @brief Returns, but does not remove, the first element from the queue.
  *
  * @param rq The resend_queue to search through.
  *
- * @return The resend element first in the queue or \c NULL if the queue if
- * empty.
+ * @return The resend element first in the queue.
+ * @retval NULL if the queue is empty.
  */
 struct resend_elem *firefly_resend_top(struct resend_queue *rq);
 
 /**
- * @brief TODO
+ * @brief Wait until it is time to resend the next packet and get the data
+ * associated with that packet.
+ *
+ * @param rq The resend queue to wait on packets in.
+ * @param data Result parameter, a pointer to a pointer of data. The memory will
+ * be allocated and set by this function.
+ * @param size Result parameter, a pointer to the variable the size of the data
+ * will be saved in.
+ * @param conn Result parameter. The pointed to pointer will be set to the
+ * connection the returned data shall be sent on.
+ * @param id Result parameter. The ID of the packet returned.
+ * @return Integer indicating result.
+ * @retval 0 If a packet was returned and should be sent again.
+ * @retval <0 If a packet was found but its number of retries was exceeded. In
+ * this case the parameters \p data, \p size, \p id will be set to \c NULL, \c
+ * 0, \c 0 respectively and the parameter \p conn will be set to the connection
+ * the packet was associated with.
+ * @note The packet will remain in its place in the queue after this operation.
+ * @see #firefly_resend_readd()
  */
 int firefly_resend_wait(struct resend_queue *rq,
 		unsigned char **data, size_t *size, struct firefly_connection **conn,
 		unsigned char *id);
 
 /**
- * @brief TODO
+ * @brief Add a timeout to a packet already in the resend queue.
  *
  * Add to queue again, add timeout to resend_at, return -1 if retries reached 0
+ * @param rq The resend queue the packet is in.
+ * @param id The id of the packet to add the timeout to.
+ * @param timeout_ms The timeout in ms to add to the packet.
+ *
+ * @note This function must be called after calling #firefly_resend_wait() to
+ * push the packet to the back of the queue.
+ * @see #firefly_resend_wait()
  */
 void firefly_resend_readd(struct resend_queue *rq, unsigned char id,
 		long timeout_ms);
