65689e2c30ff22f63d892547375f19e4e6b8c933
Tommy Olofsson <tommy.olofsson.90@gmail.com>
2014-05-21 16:34:49 +0200

    Changed how labcomm is located.

--- src/Toolchain-vxworks.cmake
+++ src/Toolchain-vxworks.cmake
@@ -4,6 +4,11 @@ set(CMAKE_SYSTEM_NAME Generic)
 
 set(VXWORKS_COMPILING TRUE)
 
+if("$ENV{LABCOMM_VX}" STREQUAL "")
+	message(FATAL_ERROR "Set env. variable LABCOMM_VX to labcomm root.")
+endif()
+set(LABCOMM_ROOT_DIR $ENV{LABCOMM_VX}/lib/c)
+
 # Specify the cross compiler:
 set(CMAKE_C_COMPILER i586-wrs-vxworks-gcc)
 #set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DLABCOMM_COMPAT=\\\"labcomm_compat_vxworks.h\\\" -DLABCOMM_NO_STDIO")
