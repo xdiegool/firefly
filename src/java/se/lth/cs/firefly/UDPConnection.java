7ab93dc41628d13bbe08e1808ed4190e18fcee3a
Niklas Jonsson <dat11njo@student.lu.se>
2014-07-28 16:29:52 +0200

    Worked on an UDPImplementation of firefly, semi-working at this stage. A race condition to solve and som unspecified bugs.

---
+++ src/java/se/lth/cs/firefly/UDPConnection.java
@@ -0,0 +1,61 @@
+package se.lth.cs.firefly;
+
+import se.lth.control.labcomm.*;
+import genproto.*;
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+/**
+* This class handles the channel protocol and is
+* a intermediary for passing data from remote host
+* to local channels.
+*
+*/
+public class UDPConnection extends Connection{ 
+
+	
+	private DatagramSocket dsock;
+	
+
+	public UDPConnection(int localPort, FireflyApplication delegate) throws IOException{	
+		super(localPort, delegate);
+		
+	}	
+	public UDPConnection(int localPort, int remotePort, InetAddress remoteAddress, FireflyApplication delegate) throws IOException{
+		super(localPort, remotePort, remoteAddress, delegate);	
+	}
+	@Override
+	protected void init() throws IOException{
+		dsock = new DatagramSocket(localPort);
+	}
+	@Override
+	protected int bufferSize()  throws IOException{
+		return 64000;
+	}
+	@Override
+	protected int receive(byte[] inc, int len)  throws IOException{
+		DatagramPacket p = new DatagramPacket(inc, len);	
+		dsock.receive(p);
+		if(remoteAddress == null && delegate.acceptConnection(p.getAddress(), p.getPort())){
+Debug.log("Delegate accepted");
+			remoteAddress = p.getAddress(); //TODO Ask delegate if they accept packages from this host
+			remotePort = p.getPort();
+			startSending();
+		}
+		
+		return p.getLength();
+	}
+	@Override
+	protected void send(byte[] data, int length)  throws IOException{
+		String s = "";
+		for(int i = 0; i < length; i++){
+			s+=data[i] + " ";
+		}
+		dsock.send(new DatagramPacket(data, length, remoteAddress,  remotePort));
+	}
+	@Override
+	protected void closeTransport(){
+		dsock.close();
+	}
+}
